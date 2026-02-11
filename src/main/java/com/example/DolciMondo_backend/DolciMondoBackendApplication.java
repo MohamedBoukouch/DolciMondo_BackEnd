9ae353dc13be945cb09ce8da509608aa975609d6
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2026-02-11 21:10:10 +0100

    first commit ✌

---
+++ src/main/java/com/example/DolciMondo_backend/DolciMondoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DolciMondo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolciMondoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolciMondoBackendApplication.class, args);
+	}
+
+}
